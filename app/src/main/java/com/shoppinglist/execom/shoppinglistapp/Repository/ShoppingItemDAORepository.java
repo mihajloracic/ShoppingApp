c4963db2143e7de5730956077f66aa3d34ff73c0
mihajlo <mihajlo.rac@gmail.com>
2017-04-12 19:59:58 +0200

    smothing up list view

---
+++ app/src/main/java/com/shoppinglist/execom/shoppinglistapp/Repository/ShoppingItemDAORepository.java
@@ -0,0 +1,11 @@
+package com.shoppinglist.execom.shoppinglistapp.Repository;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+/**
+ * Created by mihajlo on 4/12/2017.
+ */
+
+public class ShoppingItemDAORepository {
+    //private Firebase firebase;
+}
